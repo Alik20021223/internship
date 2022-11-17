17a5da4d8790d86702aeae756c29e85fe600be83
Alisher <alik20021223@gmail.com>
2022-11-17 17:50:58 +0500

    with all

---
+++ Xiaomi Himo/start/js/main.js
@@ -0,0 +1,16 @@
+$(function (){
+    $('.bike-slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $(".menu, .bike__column__text").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
